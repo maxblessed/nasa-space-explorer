9935522c45748a744bdd74bcf4e4de1d8f18802b
Maxwell <maxwemade@gmail.com>
2026-03-22 23:20:16 +0000

    fix render url

--- api/src/app.ts
+++ api/src/app.ts
@@ -5,7 +5,14 @@ import apodRoutes from './modules/apod/apod.routes'
 
 const app = express()
 
-app.use(cors())
+app.use(
+  cors({
+    origin: [
+      'http://localhost:5173',
+      'https://nasa-space-explorer-blue.vercel.app',
+    ],
+  }),
+)
 app.use(express.json())
 
 // health check
