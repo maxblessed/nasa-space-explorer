af0484896269ec2fe67f6d31885dfedafdbe6bbf
Maxwell <maxwemade@gmail.com>
2026-03-22 23:18:27 +0000

    testing vercel url

--- web/src/api/nasaApi.ts
+++ web/src/api/nasaApi.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 const API_URL = import.meta.env.VITE_API_URL
 console.log('VITE_API_URL:', import.meta.env.VITE_API_URL)
-const API = API_URL ? API_URL + 'api' : 'http://localhost:10000/api'
+const API = API_URL ? API_URL + '/api' : 'http://localhost:10000/api'
 export const api = axios.create({
   baseURL: API,
 })
